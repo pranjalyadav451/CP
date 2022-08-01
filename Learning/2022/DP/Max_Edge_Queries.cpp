054527e4884da2eb3e37380fd20393148a651368
pranjalyadav451 <2019uec1008@mnit.ac.in>
2022-08-01 06:40:51 +0530

    1 July 2022 - Alright

---
+++ Learning/2022/DP/Max_Edge_Queries.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ *  Finding the max edge in a path between two vertices 'u' and 'v' in a tree.
+ *  Use of Binary Lifting
+ * */
+
+void solve(){
+  
+}
+
+int main(){
+  int tt;
+
+  while(tt--){
+    solve();
+  }
+}
